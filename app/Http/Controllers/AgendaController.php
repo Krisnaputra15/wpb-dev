3194a9fe46268926777ab1cc41dfbc502ef6c585
Krisnaputra15 <krisnap2002@gmail.com>
2025-07-22 07:33:36 +0700

    temp

--- app/Http/Controllers/AgendaController.php
+++ app/Http/Controllers/AgendaController.php
@@ -224,9 +224,9 @@ class AgendaController extends Controller
                 Storage::disk('public')->delete($agenda->cover);
                 // unlink(public_path($agenda->cover));
             }
-            BoothTransaction::whereIn('id', RegisteredBooth::where('agenda_id', $id)->select(['booth_transaction_id'])->get()->pluck('booth_transaction_id'))->delete();
             RegisteredBooth::where('agenda_id', $id)->delete();
             AgendaParticipant::where('agenda_id', $id)->delete();
+            BoothTransaction::whereIn('id', RegisteredBooth::where('agenda_id', $id)->select(['booth_transaction_id'])->get()->pluck('booth_transaction_id'))->delete();
             $agenda->delete();
             return response()->json(['message' => 'Agenda berhasil dihapus'], 200);
         } else {
